da866116d52319b98490f507bbab8033d0dee01f
Robert Laws <roblaws@me.com>
2018-12-09 21:42:02 +0300

    update style, add hide/show functionality

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,14 @@
 // alert('hello');
+
+function showDetails(element) {
+  // var currentNodeClass = element.parentNode.getAttribute('class');
+  // var detailsNode = document.getElementsByClassName(currentNodeClass).item(0).nextElementSibling;
+
+  var detailsNode = element.parentNode.parentNode.getElementsByTagName('div').item(element.parentNode.parentNode.getElementsByTagName('div').length - 1);  
+
+  if (detailsNode.getAttribute('style')) {
+    detailsNode.removeAttribute('style');
+  } else {
+    detailsNode.setAttribute('style', 'display: block');
+  }  
+}
