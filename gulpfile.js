469d5a3f06a51674a27bd2dcd834ea43ac75a955
Robert Laws <roblaws@me.com>
2019-01-24 21:26:45 +0300

    refactor gulpfile and sass structure

--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,34 @@
 var gulp = require("gulp");
 var pug = require("gulp-pug");
 var sass = require("gulp-sass");
+var cleanCSS = require('gulp-clean-css')
+var concat = require('gulp-concat')
 var prefix = require("gulp-autoprefixer");
 var sourcemaps = require("gulp-sourcemaps");
 var del = require("del");
-var browserSync = require("browser-sync");
+var browserSync = require("browser-sync").create();
 
 var paths = {
   styles: {
     src: "src/sass/**/*.scss",
-    dest: "build/styles"
+    dest: "dist/styles"
   },
   pug: {
     src: "src/pug/*.pug",
     watch: "src/pug/**/*.pug",
-    dest: "build"
+    dest: "dist"
   },
   scripts: {
     src: "src/js/**/*.js",
-    dest: "build/scripts"
+    dest: "dist/scripts"
   },
   images: {
-    src: ["src/images/**/*.jpg", "src/images/**/*.JPG", "src/images/**/*.png", "src/images/**/*.ico", "src/images/**/*.svg"],
-    dest: "build/img"
+    src: ["src/img/**/*.jpg", "src/img/**/*.JPG", "src/img/**/*.png", "src/img/**/*.ico", "src/img/**/*.json"],
+    dest: "dist/images"
+  },
+  fonts: {
+    src: "src/fonts/**/*",
+    dest: "dist/fonts"
   }
 }
 
@@ -34,53 +40,85 @@ var prefixerOptions = {
   browsers: ['last 2 versions']
 };
 
-gulp.task("pug", function() {
-  return gulp.src(paths.pug.src)
+gulp.task('clean', function() {
+  return del(['dist']);
+});
+
+gulp.task('pug', function() {
+  var stream = gulp.src(paths.pug.src)
     .pipe(pug({
       pretty: true
     }))
-    .pipe(gulp.dest(paths.pug.dest))
-    .pipe(browserSync.stream())    
+    .pipe(gulp.dest(paths.pug.dest));
+  return stream;
 });
 
-gulp.task("sass", function() {
-  return gulp.src(paths.styles.src)
+gulp.task('styles', function() {
+  var stream = gulp.src(paths.styles.src)
     .pipe(sourcemaps.init())
     .pipe(sass(sassOptions).on("error", sass.logError))
     .pipe(prefix(prefixerOptions))
     .pipe(sourcemaps.write())
-    .pipe(gulp.dest(paths.styles.dest))
-    .pipe(browserSync.stream());
+    .pipe(gulp.dest(paths.styles.dest));
+  return stream;
 });
 
-gulp.task("scripts", function() {
-  return gulp.src(paths.scripts.src)
-    .pipe(gulp.dest(paths.scripts.dest))
-    .pipe(browserSync.stream())
+gulp.task('scripts', function() {
+  var stream = gulp.src(paths.scripts.src)
+    .pipe(gulp.dest(paths.scripts.dest));
+  return stream;
 });
 
-gulp.task("images", function() {
-  return gulp.src(paths.images.src)
-    .pipe(gulp.dest(paths.images.dest))
-    .pipe(browserSync.stream())
+gulp.task('images', function() {
+  var stream = gulp.src(paths.images.src)
+    .pipe(gulp.dest(paths.images.dest));
+  return stream;
 });
 
-gulp.task("clean", function() {
-  return del(["build"])
+gulp.task('fonts', function() {
+  var stream = gulp.src(paths.fonts.src)
+    .pipe(gulp.dest(paths.fonts.dest));
+  return stream;
 });
 
-gulp.task("watch", function() {
+gulp.task('browser', function() {
   browserSync.init({
     server: {
-      baseDir: "./build"
+      baseDir: "./dist"
     }
   });
-  gulp.watch(paths.styles.src, ["sass"]);
-  gulp.watch(paths.pug.watch, ["pug"]);
-  gulp.watch(paths.scripts.src, ["scripts"]);
-  gulp.watch(paths.images.src, ["images"]);
+
+  gulp.watch(paths.pug.watch, { usePolling: true, interval: 250 }, gulp.parallel('pug'))
+    .on('change', browserSync.reload);
+
+  // watch and rebuild .js files
+  gulp.watch(paths.scripts.src, { usePolling: true, interval: 250 }, gulp.parallel('scripts'))
+    .on('change', browserSync.reload);
+ 
+  // watch and rebuild .css files
+  gulp.watch(paths.styles.src, { usePolling: true, interval: 250 }, gulp.parallel('styles'))
+    .on('change', browserSync.reload);
+ 
+  // Reload when html changes
+  gulp.watch(paths.images.src, { usePolling: true, interval: 250 }, gulp.parallel('images'))
+    .on('change', browserSync.reload);
 });
 
-gulp.task("build", ["sass", "pug", "scripts", "images"]);
+gulp.task('build', gulp.series('clean',
+  gulp.parallel(
+    'pug',
+    'fonts',
+    'styles',
+    'scripts',
+    'images')));
 
-gulp.task("default", ["sass", "pug", "scripts", "images", "watch"]);
+gulp.task('serve', gulp.series('clean',
+  gulp.parallel(
+    'pug',
+    'fonts',
+    'styles',
+    'scripts',
+    'images'),
+  'browser'));
+
+gulp.task('default', gulp.series('serve'));
